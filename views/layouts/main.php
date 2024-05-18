bc6e8768b07c1dbd35493033f1634b5c894688d5
Bohdan <zikbert77@gmail.com>
2024-05-18 17:12:47 +0200

    bootstrap 5

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -26,8 +26,8 @@ $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
 
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -38,17 +38,37 @@ $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii
 <?php $this->beginBody() ?>
 
 <header>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+<!--    <nav class="navbar navbar-expand-lg navbar-light bg-light">-->
+<!--        <div class="container">-->
+<!--            <a class="navbar-brand" href="/">Camp</a>-->
+<!--            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">-->
+<!--                <span class="navbar-toggler-icon"></span>-->
+<!--            </button>-->
+<!--            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">-->
+<!--                <div class="navbar-nav mr-auto">-->
+<!--                    <a class="nav-item nav-link" href="#">About</a>-->
+<!--                </div>-->
+<!--                <a class="nav-item nav-link btn-custom" href="/donation">Make a Donation</a>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </nav>-->
+
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <a class="navbar-brand" href="#">Camp</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav mr-auto">
-                    <a class="nav-item nav-link" href="#">About</a>
-                </div>
-                <a class="nav-item nav-link btn-custom" href="#">Make a Donation</a>
+            <div class="collapse navbar-collapse" id="navbarText">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/about">About Us</a>
+                    </li>
+                </ul>
+                <a class="nav-item nav-link btn-custom big" href="/donation">Make a Donation</a>
             </div>
         </div>
     </nav>
@@ -61,7 +81,7 @@ $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii
 <footer id="footer" class="mt-auto py-3 bg-light">
     <div class="container">
         <div class="row text-muted">
-            <div class="col-xs-12 col-md-4 text-center text-md-start order-3 order-md-0">
+            <div class="col-xs-12 col-md-4 text-center text-md-start order-3 order-md-1">
                 <div class="info">
                     <div class="brand">
                         &copy; My Company <?= date('Y') ?><br>
@@ -77,7 +97,7 @@ $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii
                     </div>
                 </div>
             </div>
-            <div class="col-xs-12 col-md-4 text-center text-md-end">
+            <div class="col-xs-12 col-md-4 text-center order-1 order-md-2">
                 <div class="contacts">
                     <div class="header">
                         <h4>Our Contacts</h4>
@@ -90,7 +110,7 @@ $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii
                     <div class="social-networks"></div>
                 </div>
             </div>
-            <div class="col-xs-12 col-md-4 text-right order-2">
+            <div class="col-xs-12 col-md-4 text-right order-2 order-md-3">
                 <div class="links">
                     <div class="link">
                         <a href="" class="btn-custom big">Make a donation</a>
