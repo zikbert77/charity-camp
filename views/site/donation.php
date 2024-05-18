4340802ca6aa2b51d71fe1c496887b9a946158f1
Bohdan <zikbert77@gmail.com>
2024-05-18 15:07:57 +0200

    donations page

--- views/site/donation.php
+++ views/site/donation.php
@@ -2,43 +2,100 @@
 /**
  * @var \yii\web\View $this
  */
-
-$imgList = [
-    '/img/childrens6.jpg',
-    '/img/childrens6.jpg',
-    '/img/childrens6.jpg',
-    '/img/childrens6.jpg',
-    '/img/childrens6.jpg',
-    '/img/childrens6.jpg',
-    '/img/childrens6.jpg',
-    '/img/childrens6.jpg',
-    '/img/childrens6.jpg',
-    '/img/childrens6.jpg',
-    '/img/childrens6.jpg',
-    '/img/childrens6.jpg',
-];
 ?>
 
-<style>
-    .splide__slide img {
-        width: 300px;
-    }
-</style>
-
-<div class="container">
-    <div class="row">
-        <div class="col col-12">
-            <div class="splide">
-                <div class="splide__track">
-                    <ul class="splide__list">
-                        <?php foreach ($imgList as $img): ?>
-                            <li class="splide__slide">
-                                <img src="<?= $img ?>" alt="">
-                            </li>
-                        <?php endforeach; ?>
-                    </ul>
+<section id="donation-methods">
+    <div class="container">
+        <div class="row">
+            <div class="col col-12">
+                <h2>Donate with Crypto: Empower Children's Futures and Make a Difference Today</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col col-3">
+                <div class="donation-method shadow">
+                    <div class="logo">
+                        <img src="/img/donation-methods/eth.png" alt="">
+                    </div>
+                    <div class="title">
+                        ETH
+                    </div>
+                    <div class="wallet">
+                        <span class="link">0x8cDa69814C54BEcF3633EcB5f5352d31fA497aDd</span>
+                        <span class="action-copy-wallet-address" data-bs-toggle="tooltip" data-bs-placement="top"
+                              data-bs-custom-class="custom-tooltip"
+                              data-bs-title="Copy wallet address">
+                            <i class="icofont-duotone icofont-duplicate-alt"></i>
+                        </span>
+                    </div>
+                    <div class="qr-code">
+                        <img src="/img/donation-methods/qr-eth.jpeg" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="col col-3">
+                <div class="donation-method shadow">
+                    <div class="logo">
+                        <img src="/img/donation-methods/ftm.png" alt="">
+                    </div>
+                    <div class="title">
+                        FTM
+                    </div>
+                    <div class="wallet">
+                        <span class="link">0x030f3BAd9c445C962FcD8C8eA112366Eb6eF379a</span>
+                        <span class="action-copy-wallet-address" data-bs-toggle="tooltip" data-bs-placement="top"
+                              data-bs-custom-class="custom-tooltip"
+                              data-bs-title="Copy wallet address">
+                            <i class="icofont-duotone icofont-duplicate-alt"></i>
+                        </span>
+                    </div>
+                    <div class="qr-code">
+                        <img src="/img/donation-methods/qr.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="col col-3">
+                <div class="donation-method shadow">
+                    <div class="logo">
+                        <img src="/img/donation-methods/avax.png" alt="">
+                    </div>
+                    <div class="title">
+                        AVAX
+                    </div>
+                    <div class="wallet">
+                        <span class="link">0x8cDa69814C54BEcF3633EcB5f5352d31fA497aDd</span>
+                        <span class="action-copy-wallet-address" data-bs-toggle="tooltip" data-bs-placement="top"
+                              data-bs-custom-class="custom-tooltip"
+                              data-bs-title="Copy wallet address">
+                            <i class="icofont-duotone icofont-duplicate-alt"></i>
+                        </span>
+                    </div>
+                    <div class="qr-code">
+                        <img src="/img/donation-methods/qr-avax.jpeg" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="col col-3">
+                <div class="donation-method shadow">
+                    <div class="logo">
+                        <img src="/img/donation-methods/xrp.png" alt="">
+                    </div>
+                    <div class="title">
+                        XRP
+                    </div>
+                    <div class="wallet">
+                        <span class="link">rUbQpHNhC2WJWFupkTdrntDT9GqtVZwaWy</span>
+                        <span class="action-copy-wallet-address" data-bs-toggle="tooltip" data-bs-placement="top"
+                              data-bs-custom-class="custom-tooltip"
+                              data-bs-title="Copy wallet address">
+                            <i class="icofont-duotone icofont-duplicate-alt"></i>
+                        </span>
+                    </div>
+                    <div class="qr-code">
+                        <img src="/img/donation-methods/qr-xrp.jpeg" alt="">
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+</section>
--- web/js/index.js
+++ web/js/index.js
@@ -1,9 +1,37 @@
 $(document).ready(function() {
-    let splide = new Splide( '.splide', {
-        perPage: $(window).width() < 600 ? 1 : 3,
-        perMove: 1,
-        rewind : true,
-    } );
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
-    splide.mount();
+    if ($(document).find('.splide').length) {
+        let splide = new Splide('.splide', {
+            perPage: $(window).width() < 600 ? 1 : 3,
+            perMove: 1,
+            rewind : true,
+        } );
+
+        splide.mount();
+    }
+
+    $(document).on('click', '.action-copy-wallet-address', function () {
+        let $this = $(this);
+        let text = $this.parent().find('.link').text();
+
+        try {
+            let $temp = $("<input>");
+            $("body").append($temp);
+            $temp.val(text).select();
+            document.execCommand("copy");
+            $temp.remove();
+
+            let $copyIcon = $this.find('i');
+
+            $copyIcon.removeClass('icofont-duotone icofont-duplicate-alt');
+            $copyIcon.addClass('icofont-tick-mark');
+            setTimeout(function() {
+                $copyIcon.addClass('icofont-duotone icofont-duplicate-alt');
+            }, 2500);
+        } catch (e) {
+            console.error(e.error)
+        }
+    })
 });
